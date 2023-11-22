b221dc81ba351c051fc9ea31ada2740bc55b3dc6
Yeasin0011 <yeasin.ibne.kadir@g.bracu.ac.bd>
2023-11-23 03:39:58 +0600

    Add-post page added

--- ISO/ISO_Blog/views.py
+++ ISO/ISO_Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, DetailView 
+from django.views.generic import ListView, DetailView, CreateView 
 
 from .models import Post
 # Create your views here.
@@ -15,3 +15,9 @@ class Homeview(ListView):
 class postDetailview(DetailView):
     model=Post
     template_name='post.details.html'
+
+class postAddview(CreateView):
+    model=Post
+    template_name='post_add.html'
+    fields=('__all__')
+    # fields=('Title', 'Body')
