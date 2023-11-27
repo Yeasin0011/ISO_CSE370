b0d53778abc5242d7cd201add27d0fc27d7464ca
En4n <uchiha.enan@gmail.com>
2023-11-28 03:08:53 +0600

    Sadi
    edit profile, adding image, article snippets.

--- ISO/ISO/settings.py
+++ ISO/ISO/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ISO_Blog',
     'members',
+    'ckeditor',
 
 ]
 
@@ -119,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
