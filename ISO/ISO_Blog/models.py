acbb05fca072ba66a88baf955f03624a0f936265
Fuad-Rafi <firafi75@gmail.com>
2023-12-03 02:15:09 +0600

    profile page add

--- ISO/ISO_Blog/models.py
+++ ISO/ISO_Blog/models.py
@@ -21,6 +21,18 @@ class Category(models.Model):
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User,null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True,blank=True,upload_to='images/profile')
+    website =models.CharField(null=True,blank=True,max_length=255)
+    facebook_url=models.CharField(null=True,blank=True,max_length=255)
+    instagram_url=models.CharField(null=True,blank=True,max_length=255)
+    
+    def __str__(self):
+        return str(self.user)
+
+
 class Post(models.Model):
     title=models.CharField(max_length=255)
     title_tag=models.CharField(max_length=255)
