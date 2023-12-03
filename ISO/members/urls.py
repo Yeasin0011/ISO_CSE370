681dc7e4469c2568e0af50a8e28a92a7e3330245
Fuad-Rafi <firafi75@gmail.com>
2023-12-03 23:04:11 +0600

    profile page added

--- ISO/members/urls.py
+++ ISO/members/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import UserRegistrationView
+from .views import UserRegistrationView , ShowProfilePageView
 
 
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name="register"), 
-    
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profilepage'),
 ]
--- ISO/members/views.py
+++ ISO/members/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render , get_object_or_404
 
 from django.views import generic
 
+from django.views.generic import DetailView
+
 from django.contrib.auth.forms import UserCreationForm
 
 from django.urls import reverse_lazy
+
+from ISO_Blog.models import Profile
+
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+    def get_context_data(self, *args, **kwargs):
+        cat_menu = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+
+        page_user = get_object_or_404(Profile, id = self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
+    
+
 
 
 class UserRegistrationView(generic.CreateView):
