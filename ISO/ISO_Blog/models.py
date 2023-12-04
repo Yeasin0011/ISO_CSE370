0f060ce352c9d1e5530df5ccfca2ff7ab67e17d8
Fuad-Rafi <firafi75@gmail.com>
2023-12-04 16:27:00 +0600

    pofile model implemented

--- ISO/ISO_Blog/models.py
+++ ISO/ISO_Blog/models.py
@@ -23,9 +23,18 @@ class Category(models.Model):
 
 # Create your models here.
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+    profile_pic = models.ImageField(null=True, blank=True, upload_to="images/")
+    fb_url =models.CharField(max_length=255, )
+
+    def __str__(self):
+        return str(self.user)
+
 class Post(models.Model):
     title=models.CharField(max_length=255)
-    header_image= models.ImageField(null=True, blank=True, upload_to="images/")
+    header_image= models.ImageField(null=True, blank=True, upload_to="images/profile")
     title_tag=models.CharField(max_length=255)
     author=models.ForeignKey(User, on_delete=models.CASCADE)
     #body=models.TextField() 
