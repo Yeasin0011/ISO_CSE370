59d07b50a18e8a728735f56e17f71e51f8db4f42
Fuad-Rafi <firafi75@gmail.com>
2023-12-10 00:47:42 +0600

    Account Settings And Profile Settings Seperated

--- ISO/members/views.py
+++ ISO/members/views.py
@@ -29,9 +29,8 @@ class CreateProfilePageView(generic.CreateView):
 class EditProfilePageView(generic.UpdateView):
     model = Profile
     template_name = 'registration/edit_profile_page.html'
-    fields = ['bio', 'profile_pic', 'website_url', 'facebook_url', 'twitter_url', 'instagram_url']
-    success_url=reverse_lazy('home')
-
+    fields = ['bio', 'profile_pic']
+    success_url=reverse_lazy('Home')
 
 class ShowProfilePageView(DetailView):
     model = Profile
